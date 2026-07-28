f998d07551328837550d11c5764a85498047dbb0
takeda-akihiro <akihirotakeda1111@gmail.com>
2026-07-28 13:52:46 +0900

    fix(backend):アップロードに失敗する不具合を修正

--- backend/utils/services/s3.py
+++ backend/utils/services/s3.py
@@ -4,10 +4,13 @@ from django.conf import settings
 
 class S3Service:
     def __init__(self):
+        endpoint = f"https://s3.{settings.AWS_S3_REGION}.amazonaws.com"
+
         if settings.AWS_ACCESS_KEY_ID:
             self.s3 = boto3.client(
                 "s3",
                 region_name=settings.AWS_S3_REGION,
+                endpoint_url=endpoint,
                 aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             )
@@ -15,6 +18,7 @@ class S3Service:
             self.s3 = boto3.client(
                 "s3",
                 region_name=settings.AWS_S3_REGION,
+                endpoint_url=endpoint,
             )
 
         self.bucket = settings.AWS_S3_UPLOAD_BUCKET
